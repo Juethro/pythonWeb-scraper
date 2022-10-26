36ded317852ec3025e890b137870ed922389fa85
Juethroo <denisjethro@gmail.com>
2022-10-26 20:16:58 +0700

    update bug scrapy

--- src/ps_crawl/ps_crawl/spiders/play_spider.py
+++ src/ps_crawl/ps_crawl/spiders/play_spider.py
@@ -2,13 +2,13 @@ import scrapy
 
 class Ayomain_Spider(scrapy.Spider):
     name = 'ayomain'
-    start_urls = ["https://store.playstation.com/en-id/category/29696e1b-a942-4832-935d-ebd11b163263/1"]
+    start_urls = ["https://store.playstation.com/en-id/category/29696e1b-a942-4832-935d-ebd11b163263/"]
     
 
     def parse(self, response):
         url = response.url
         
-        for i in range(1,46):
+        for i in range(1,47):
             yield scrapy.Request(url=url+str(i), callback=self.parse_details)
     def parse_details(self, response):
         for text in response.css(".psw-product-tile__details"):
